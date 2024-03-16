c6e59a4469a77b91426b0172398912adff83c59f
Kallz29 <81845884+Kallz29@users.noreply.github.com>
2024-03-16 12:01:39 +0700

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,23 @@
+
+
+function Introduction() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+            <h1>
+                TEAM PROFILE
+            </h1>
+            <p>Haikal</p>
+            <p>Rafa</p>
+            <p>Alika</p>
+            <p>Tasya</p>
+        </div>
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default Introduction;
