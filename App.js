dfbd50012833d414276dd0fad0f78f20686e31b9
Kallz29 <81845884+Kallz29@users.noreply.github.com>
2024-03-17 20:17:06 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+  import './App.css';
+  import Introduction from './components/introduction';
+  import Navbar from './components/navbar';
+  import Footer from './components/footer';
+  import Desc from './components/desc';
+
+  function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+       
+        </header>
+      </div>
+    );
+  }
+
+  export default App;
+
