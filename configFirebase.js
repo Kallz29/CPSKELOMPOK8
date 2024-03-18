bd8b2b616c8dd8de3a3041d479e9cb770247d0b3
alikawiduri <148477565+alikawiduri@users.noreply.github.com>
2024-03-18 14:25:43 +0700

    Update configFirebase.js

--- configFirebase.js
+++ configFirebase.js
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// import { getAnalytics } from "firebase/analytics";
-// import configFirebase from './components/configFirebase';
 
 const configFirebase = {
   apiKey: "AIzaSyDMZ4or0lpKn0u3SLCs8uQQxN_wHCyt2B8",
@@ -13,20 +10,10 @@ const configFirebase = {
   messagingSenderId: "383524690299",
   appId: "1:383524690299:web:ff87066006d3d2bebe05de",
   measurementId: "G-KHWY53M04K"
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(configFirebase);
 const db = getDatabase(app);
-// const analytics = getAnalytics(app);
 
 export{ db };
 export default app;
