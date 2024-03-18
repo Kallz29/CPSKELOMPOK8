a9e687e431a0e855763e301ee3dbea338b2bfa3b
alikawiduri <148477565+alikawiduri@users.noreply.github.com>
2024-03-18 14:24:17 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
 import './App.css';
-  import Introduction from './components/introduction';
-  import Navbar from './components/navbar';
-  import Footer from './components/footer';
-  import Desc from './components/desc';
+import Introduction from './components/introduction';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Desc from './components/desc';
+import SensorData from './components/read';
 
-  function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Navbar />
-          <div className="content-wrapper">
-            <div id="introduction"></div>
-            <Introduction />
-            <hr className="divider" />
-            <div id="desc"></div> 
-            <Desc />
-          </div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <div className="content-wrapper">
+          <div id="introduction"></div>
+          <Introduction />
           <hr className="divider" />
-    
-          <Footer />
-        </header>
-      </div>
-    );
-  }
+          <div id="desc"></div> =
+          <Desc />
+        </div> 
+        <SensorData /> 
+        <hr className="divider" />
+        <Footer />
+      </header>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
+
